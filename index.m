0cdfc946a39e120d9e39236ccd4595b46a25b8e5
helyousi <hamza.el_yousfi@bordeaux-inp.fr>
2021-12-08 11:11:59 +0100

    index update

--- index.m
+++ index.m
@@ -1,5 +1,27 @@
 clc, clear all, close all
-%loading files to matrices
+%% loading files to matrices
 data_ds = readmatrix('mv_ds.xlsx');
 data_fs = readmatrix('mv_fs.xlsx');
-%Comparaison des performances
+%% Comparaison des performances
+differenceSad = 0;
+sadDs = zeros(1, 5680*23);
+sadFs = zeros(1, 5680*23);
+c = 1;
+for i=1:5680
+    for j=1:3:67
+        sadFs(c) = data_fs(i,j);
+        sadDs(c) = data_ds(i,j);
+        c = c + 1;
+    end
+end
+%Delete NAN values
+for i=1:5680*23
+    if isnan(sadDs(i))
+        sadDs(i) = 0;
+    if isnan(sadFs(i))
+        sadFs(i) = 0;
+    end
+    end
+end
+%compute sum of SADs
+sumSads = sum(sadDs - sadFs);
